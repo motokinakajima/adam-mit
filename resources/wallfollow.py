471f7bbb36d28f1066e9aa866b0b0b109f114b2d
Motoki Nakajima <motokinakajima0912@gmail.com>
2024-07-29 06:58:29 -0400

    added env file

--- resources/wallfollow.py
+++ resources/wallfollow.py
@@ -1,7 +1,12 @@
 import numpy as np
 from pid import PIDController
+import os
+from dotenv import load_dotenv
+from pathlib import Path
 
-
+dotenv_path = Path('../.env')
+load_dotenv(dotenv_path=dotenv_path)
+LIBRARY_PASS = os.getenv('LIBRARY_PASS')
 
 def lidar_angle(scan,angle):
     front = 90 - angle
@@ -19,8 +24,17 @@ def lidar_angle(scan,angle):
 
 class WallFollow:
     def __init__(self, angle_kp, angle_ki ,angle_kd, dist_kp, dist_ki ,dist_kd, speed, goal_dist):
+        self.angle_kp = angle_kp
+        self.angle_ki = angle_ki
+        self.angle_kd = angle_kd
+        self.dist_kp = dist_kp
+        self.dist_ki = dist_ki
+        self.dits_kd = dist_kd
         self.angle_pid = PIDController(angle_kp, angle_ki, angle_kd)
         self.dist_pid = PIDController(dist_kp, dist_ki, dist_kd)
+        
+        self.angle_pid.start()
+        self.dist_pid.start()
 
         self.speed = speed
         self.goal_dist = goal_dist
@@ -33,17 +47,20 @@ class WallFollow:
         _, forward_wall_dist = rc_utils.get_lidar_closest_point(scan, (-5, 5))
         _, r_min = rc_utils.get_lidar_closest_point(scan, (90, 270))
 
-        error_angle = lidar_angle(scan, 45)
-
+        error_angle = self.angle_pid.update(0,lidar_angle(scan, 45))
+        
+        """
         if abs(error_angle - self.prev) > 20:
-            error_a1 = self.prev 
+            error_a1 = self.prev
+        """
+        
         error_angle = np.nan_to_num(error_angle)
         self.prev = error_angle
+        
+        angle_a = self.angle_pid.update(0,lidar_angle(scan, 45))
+        angle_d = self.dist_pid.update(self.goal_dis,r_min)
 
-        error_dist = (r_min - self.goal_dist) / self.goal_dist
-
-        angle_a = np.tanh(error_angle)
-        angle_d = np.tnah(error_dist)
+        angle_d = np.tnah(angle_d)
 
         angle = angle_a + angle_d
         angle = np.clip(angle, -1, 1)
